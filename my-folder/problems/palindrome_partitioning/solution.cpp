53e295792fcf243cda711e51578d950c81bf7f36
Hyunchol Jun <94641590+hyunchol-jun@users.noreply.github.com>
2022-02-19 23:36:51 +0000

    Sync LeetCode submission - Palindrome Partitioning (cpp)

--- my-folder/problems/palindrome_partitioning/solution.cpp
+++ my-folder/problems/palindrome_partitioning/solution.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<vector<string>> result {};
-        vector<string> elements {};
-        recurPartition(s, result, elements, 0);
+        vector<vector<string>> result{};
+        vector<string> path{};
+        int size = s.size();
+        recursiveBackTrack(s, result, path, 0);
         return result;
     }
 
 private:
-    void recurPartition(string s, vector<vector<string>>& result, vector<string>& elements, int start) {
-        int size = s.length();
-        if (size <= start) {
-            result.push_back(elements);
-        }
-        
-        for (int end = start; end < size; ++end)
+    void recursiveBackTrack(const std::string& s, vector<vector<string>>& result, vector<string>& path, int start)
+    {
+        if (start == s.size()) result.push_back(path);
+        else
         {
-            if (isPalindrome(s, start, end)) {
-                elements.push_back(s.substr(start, end - start + 1));
-                recurPartition(s, result, elements, end + 1);
-                elements.pop_back();
+            for (int i = start; i < s.size(); ++i)
+            {
+                if (isPalindrome(s, start, i))
+                {
+                    path.push_back(s.substr(start, i - start + 1));
+                    recursiveBackTrack(s, result, path, i + 1);
+                    path.pop_back();
+                }
             }
         }
     }
     
-    bool isPalindrome(string& s, int low, int high) {
-        while (low < high) {
-            if (s[low++] != s[high--]) return false;
+    bool isPalindrome(const std::string& s, int l, int r)
+    {
+        while (l < r)
+        {
+            if (s[l++] != s[r--]) return false;
         }
         return true;
     }
